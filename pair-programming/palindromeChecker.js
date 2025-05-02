a9999e55ca1381c7e75f4baf3ae772d9f608746f
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-02 12:30:34 -0400

    updated instructions for pair programming practice exercise

--- pair-programming/palindromeChecker.js
+++ pair-programming/palindromeChecker.js
@@ -1,9 +1,37 @@
 /*
-	Write a function that takes in an array of strings and checks to see if each string is a palindrome or not. The function should print the string "yes" or "no" for each string.
+    INSTRUCTIONS
 
-    Use the driver-navigator model for pair programming: the driver should share their screen and type up the pseudocode and/or code while the navigator directs the problem-solving and coding process. The driver should participate in discussion but not take over the navigating (aka drivergating)! 
+    Practice pair programming! 
+    
+    Reminder of driver/navigator format:
+    Person A drives (types), Person B navigates (directs the problem-solving).
+    
+    Ideally you should switch roles at some point so both people 
+    get to experience both roles - this is easiest in a remote setting if
+    you use an online tool like Replit.com where you can invite others to 
+    edit the same file but it still formats it as code and can be run in 
+    a console (which you can't do in a Google doc!)
+
+    Ultimately you will write and test two functions:
+        1. Create a helper function that accepts a string and returns 
+        the string in reverse.
+        2. Your primary function should accept an array of strings. 
+        For each word in the array, you should print something like, 
+        "The word 'tacocat' is a palindrome!" 
+           or 
+        "The word 'dated' is NOT a palindrome."
+        Make use of your first function. 
+        
+        You can choose to use for loops, for/of loops, or even .forEach() 
+        in your second function!
+
+    Pseudocode your logic first, then write actual code.
+
+    You should test the first function with single strings before moving 
+    on to the second.
+
+    Here is some sample data for testing:
+    ["moon", "mom", "tacocat", "adobo", "agog", "kayak", "bikini", "banana", "level", "garage", "deified", "angora", "radar", "rotator", "dated", "solos", "tenet"]
+
 */
-
-let words = ["moon", "mom", "tacocat", "adobo", "agog", "kayak", "bikini", "banana", "level", "garage", "deified", "angora", "radar", "rotator", "dated", "solos", "tenet"];
-
 
