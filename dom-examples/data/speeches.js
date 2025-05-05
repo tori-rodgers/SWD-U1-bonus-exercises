1638f4466285bdcad83dd5804fc73e1ac530acef
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-05 10:40:36 -0400

    added starter code for first DOM demo

---
+++ dom-examples/data/speeches.js
@@ -0,0 +1,142 @@
+// Text of Dr. Martin Luther King Jr.'s transcendent speech on August 28, 1963
+
+const mlkSpeech = `Five score years ago, a great American, in whose symbolic shadow we stand today, signed the Emancipation Proclamation. This momentous decree came as a great beacon of hope to millions of slaves, who had been seared in the flames of withering injustice. It came as a joyous daybreak to end the long night of their captivity. But one hundred years later, the colored America is still not free. One hundred years later, the life of the colored American is still sadly crippled by the manacle of segregation and the chains of discrimination.
+
+One hundred years later, the colored American lives on a lonely island of poverty in the midst of a vast ocean of material prosperity. One hundred years later, the colored American is still languishing in the corners of American society and finds himself an exile in his own land So we have come here today to dramatize a shameful condition.
+
+In a sense we have come to our Nation's Capital to cash a check. When the architects of our great republic wrote the magnificent words of the Constitution and the Declaration of Independence, they were signing a promissory note to which every American was to fall heir.
+
+This note was a promise that all men, yes, black men as well as white men, would be guaranteed the inalienable rights of life liberty and the pursuit of happiness.
+
+It is obvious today that America has defaulted on this promissory note insofar as her citizens of color are concerned. Instead of honoring this sacred obligation, America has given its colored people a bad check, a check that has come back marked “insufficient funds."
+
+But we refuse to believe that the bank of justice is bankrupt. We refuse to believe that there are insufficient funds in the great vaults of opportunity of this nation. So we have come to cash this check, a check that will give us upon demand the riches of freedom and security of justice.
+
+We have also come to his hallowed spot to remind America of the fierce urgency of Now. This is not time to engage in the luxury of cooling off or to take the tranquilizing drug of gradualism.
+
+Now is the time to make real the promise of democracy.
+
+Now it the time to rise from the dark and desolate valley of segregation to the sunlit path of racial justice.
+
+Now it the time to lift our nation from the quicksand of racial injustice to the solid rock of brotherhood.
+
+Now is the time to make justice a reality to all of God's children.
+
+I would be fatal for the nation to overlook the urgency of the moment and to underestimate the determination of it's colored citizens. This sweltering summer of the colored people's legitimate discontent will not pass until there is an invigorating autumn of freedom and equality. Nineteen sixty-three is not an end but a beginning. Those who hope that the colored Americans needed to blow off steam and will now be content will have a rude awakening if the nation returns to business as usual.
+
+There will be neither rest nor tranquility in America until the colored citizen is granted his citizenship rights. The whirlwinds of revolt will continue to shake the foundations of our nation until the bright day of justice emerges.
+
+We can never be satisfied as long as our bodies, heavy with the fatigue of travel, cannot gain lodging in the motels of the highways and the hotels of the cities.
+
+We cannot be satisfied as long as the colored person's basic mobility is from a smaller ghetto to a larger one.
+
+We can never be satisfied as long as our children are stripped of their selfhood and robbed of their dignity by signs stating “for white only."
+
+We cannot be satisfied as long as a colored person in Mississippi cannot vote and a colored person in New York believes he has nothing for which to vote.
+
+No, no we are not satisfied and we will not be satisfied until justice rolls down like waters and righteousness like a mighty stream.
+
+I am not unmindful that some of you have come here out of your trials and tribulations. Some of you have come from areas where your quest for freedom left you battered by storms of persecutions and staggered by the winds of police brutality.
+
+You have been the veterans of creative suffering. Continue to work with the faith that unearned suffering is redemptive.
+
+Go back to Mississippi, go back to Alabama, go back to South Carolina go back to Georgia, go back to Louisiana, go back to the slums and ghettos of our modern cities, knowing that somehow this situation can and will be changed.
+
+Let us not wallow in the valley of despair. I say to you, my friends, we have the difficulties of today and tomorrow.
+
+I still have a dream. It is a dream deeply rooted in the American dream.
+
+I have a dream that one day this nation will rise up and live out the true meaning of its creed. We hold these truths to be self-evident that all men are created equal.
+
+I have a dream that one day out in the red hills of Georgia the sons of former slaves and the sons of former slaveowners will be able to sit down together at the table of brotherhood.
+
+I have a dream that one day even the state of Mississippi, a state sweltering with the heat of oppression, will be transformed into an oasis of freedom and justice.
+
+I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by their character.
+
+I have a dream today.
+
+I have a dream that one day down in Alabama, with its vicious racists, with its governor having his lips dripping with the words of interposition and nullification; that one day right down in Alabama little black boys and black girls will be able to join hands with little white boys and white girls as sisters and brothers.
+
+I have a dream today.
+
+I have a dream that one day every valley shall be engulfed, every hill shall be exalted and every mountain shall be made low, the rough places will be made plains and the crooked places will be made straight and the glory of the Lord shall be revealed and all flesh shall see it together.
+
+This is our hope. This is the faith that I will go back to the South with. With this faith we will be able to hew out of the mountain of despair a stone of hope.
+
+With this faith we will be able to transform the jangling discords of our nation into a beautiful symphony of brotherhood.
+
+With this faith we will be able to work together, to pray together, to struggle together, to go to jail together, to climb up for freedom together, knowing that we will be free one day.
+
+This will be the day when all of God's children will be able to sing with new meaning “My country 'tis of thee, sweet land of liberty, of thee I sing. Land where my father's died, land of the Pilgrim's pride, from every mountainside, let freedom ring!"
+
+And if America is to be a great nation, this must become true. So let freedom ring from the hilltops of New Hampshire. Let freedom ring from the mighty mountains of New York.
+
+Let freedom ring from the heightening Alleghenies of Pennsylvania.
+
+Let freedom ring from the snow-capped Rockies of Colorado.
+
+Let freedom ring from the curvaceous slopes of California.
+
+But not only that, let freedom ring from Stone Mountain of Georgia.
+
+Let freedom ring from every hill and molehill of Mississippi and every mountainside.
+
+When we let freedom ring, when we let it ring from every tenement and every hamlet, from every state and every city, we will be able to speed up that day when all of God's children, black men and white men, Jews and Gentiles, Protestants and Catholics, will be able to join hands and sing in the words of the old spiritual, “Free at last, free at last. Thank God Almighty, we are free at last."`;
+
+export const mlkData = {
+    id: 1,
+    title: "I Have a Dream",
+    speaker: "Dr. Martin Luther King, Jr.",
+    date: "August 28, 1963",
+    text: mlkSpeech,
+    imagePath: "./images/king.webp",
+};
+
+
+// Abraham Lincoln - Gettysburg Address
+
+const lincolnSpeech = `Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal. 
+
+Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting-place for those who here gave their lives, that that nation might live. It is altogether fitting and proper that we should do this. 
+
+But, in a larger sense, we cannot dedicate, we cannot consecrate—we cannot hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. 
+
+It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth.`;
+
+export const lincolnData = {
+    id: 2,
+    title: "The Gettysburg Address",
+    speaker: "President Abraham Lincoln",
+    date: "November 19, 1863",
+    text: lincolnSpeech,
+    imagePath: "./images/lincoln.jpg",
+};
+
+
+// Sojourner Truth - Ain't I a Woman?
+
+const truthSpeech = `Well, children, where there is so much racket there must be something out of kilter. I think that 'twixt the negroes of the South and the women at the North, all talking about rights, the white men will be in a fix pretty soon. But what's all this here talking about?
+
+That man over there says that women need to be helped into carriages, and lifted over ditches, and to have the best place everywhere. Nobody ever helps me into carriages, or over mud-puddles, or gives me any best place! And ain't I a woman? Look at me! Look at my arm! I have ploughed and planted, and gathered into barns, and no man could head me! And ain't I a woman? I could work as much and eat as much as a man - when I could get it - and bear the lash as well! And ain't I a woman? I have borne thirteen children, and seen most all sold off to slavery, and when I cried out with my mother's grief, none but Jesus heard me! And ain't I a woman?
+
+Then they talk about this thing in the head; what's this they call it? [member of audience whispers, "intellect"] That's it, honey. What's that got to do with women's rights or negroes' rights? If my cup won't hold but a pint, and yours holds a quart, wouldn't you be mean not to let me have my little half measure full?
+
+Then that little man in black there, he says women can't have as much rights as men, 'cause Christ wasn't a woman! Where did your Christ come from? Where did your Christ come from? From God and a woman! Man had nothing to do with Him.
+
+If the first woman God ever made was strong enough to turn the world upside down all alone, these women together ought to be able to turn it back , and get it right side up again! And now they is asking to do it, the men better let them.
+
+Obliged to you for hearing me, and now old Sojourner ain't got nothing more to say.`;
+
+export const truthData = {
+    id: 3,
+    title: "Ain't I a Woman?",
+    speaker: "Sojourner Truth",
+    date: "1851",
+    text: truthSpeech,
+    imagePath: "./images/truth.jpg",
+};
+
+// This better approximates data that would be pulled from a database through an API
+// Each key corresponds to the id in the object
+export const allData = { 1: mlkData, 2: lincolnData, 3: truthData};
---
+++ dom-examples/script1.js
@@ -0,0 +1,41 @@
+// TODO: Import mlkData
+
+window.onload = () => {
+
+    // DECLARE OBJECTS REPRESENTING HTML ELEMENTS ON PAGE
+    const pageTitle = document.querySelector("title");
+    const main = document.querySelector("main");
+
+    // TODO: Initialize three objects that represent the three elements already in <main>
+
+
+    // TODO: Use .createElement() to create an element and .append() to add it to <main>
+    // Then give it an id of "speech-box"
+
+   
+    
+    // RENDER THE PAGE
+
+    // TODO: Call the render function and pass in the data
+
+
+
+    // FUNCTIONS BELOW
+
+    // TODO: Write a function to carry out everything needed for rendering the page.
+    function renderPage(speechData) {
+
+        // TODO: Set background image
+
+
+        // TODO: Set the <title> in the <head>
+
+
+        // TODO: Set the content for all four elements in <main> using the imported data
+
+
+        // TODO: Improve the readability of the text of the speech
+
+    }
+
+}
