bdfaa593089b0815c1c9246132c67f6a9cb174c5
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-15 12:38:07 -0400

    added fetch with useEffect()

--- react/garden-planning-tool/src/App.jsx
+++ react/garden-planning-tool/src/App.jsx
@@ -1,36 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Header from './components/Header';
 import Main from './components/Main';
 import PlantInfo from './components/PlantInfo';
+import Spinner from './components/Spinner';
+import { alphabetize } from './shared/utils';
 import './App.css';
-import data from './data/data.json'; // TEMP until fetching JSON
 
 function App() {
-    
 	// State variable to hold all plant objects
-	const [allPlants, setAllPlants] = useState(
-		data.map(obj => {
+	const [allPlants, setAllPlants] = useState([]);
+
+	// This state variable will allow us to render the page
+	// only after fetched data is available
+	const [loading, setLoading] = useState(true);
+
+	const fetchPlants = async () => {
+		let response = await fetch(
+			'https://docs.google.com/document/d/1DjGYPISRGCsP4zHwJYBtciqbKAvae1CZ9EKsci4mTJk/export?format=txt'
+		);
+		let data = await response.json();
+
+		let plantData = data.map(obj => {
 			return { ...obj, numAllocated: 0 };
-		})
-	);
+		});
+
+		alphabetize(plantData, 'name');
+
+		setAllPlants(plantData);
+	};
+
+	// Runs once when component first loads
+	useEffect(() => {
+		fetchPlants();
+	}, []);
+
+	// Runs only if the state variable allPlants is updated
+	useEffect(() => {
+		if (allPlants.length > 0) {
+			setLoading(false);
+		}
+	}, [allPlants]);
 
 	return (
-        <div id="page-layout">
-            <Header />
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/plant/:id"
-                        element={<PlantInfo allPlants={allPlants} />}
-                    />
-                    <Route
-                        path="/"
-                        element={<Main allPlants={allPlants} setAllPlants={setAllPlants} />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </div>
+		<div id="page-layout">
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<>
+						<Route
+							path="/plant/:id"
+							element={<PlantInfo allPlants={allPlants} />}
+						/>
+						<Route
+							path="/"
+							element={
+								loading ? (
+									<Spinner />
+								) : (
+									<Main allPlants={allPlants} setAllPlants={setAllPlants} />
+								)
+							}
+						/>
+					</>
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
---
+++ react/garden-planning-tool/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+	return (
+		<div id="spinner-container">
+			<i id="spinner-icon" className="fa-solid fa-leaf fa-5x fa-spin"></i>
+		</div>
+	);
+};
+
+export default Spinner;
