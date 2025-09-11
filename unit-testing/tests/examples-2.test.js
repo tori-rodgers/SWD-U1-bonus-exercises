f7cf54d53eea4d4c6a5ca9b86a77a3746ca6d67e
Carolista <cjones.renaissancewoman@gmail.com>
2025-09-10 20:17:51 -0400

    set up TODOs and starter code for new unit testing examples

---
+++ unit-testing/tests/examples-2.test.js
@@ -0,0 +1,47 @@
+const examples2 = require('../examples-2.js');
+
+/* TODO: SET UP MOCK DATA */
+
+// For testing isValidPhoneNumber()
+
+
+// For testing formatPhone()
+
+// For testing roundNumberToDecimalPlace()
+
+// For testing reverseString()
+
+
+// For testing getPalindromes()
+
+
+// For testing getTitleCase()
+
+
+/* TODO: SET UP TESTS */
+
+/*
+    Group related tests in a describe() block - description should be the thing you are testing
+    Each specification is a test() block - description should clarify in what way it is being tested
+    Each test inside the specification is called an expectation - compare actual vs expected
+*/
+
+// Test isValidPhoneNumber()
+
+
+// Test formatPhone()
+
+
+// Test roundNumberToDecimalPlace()
+
+
+// Test reverseString()
+
+
+// Test getPalindromes()
+
+
+// Test getTitleCase() using TDD methodology (write test first, code next, pass test, repeat)
+
+
+// Don't forget to use 'npm test' in the terminal to view code coverage. It should be at 100% for this file now!
